d8e37995c005c678448b91e7ea9ea3088cb1adc0
KOTTEESWARAN2008 <ramusvc957@gmail.com>
2026-07-12 22:46:59 -0700

    Add files via upload

---
+++ Append.py
@@ -0,0 +1,25 @@
+A=[]
+B=[]
+c=[]
+
+for  i in range(0,5):
+    x=int(input("Enter the number of A :"))
+    A.append(x)
+
+for i in range(0,5):
+    y=int(input("Enter the Number of B :"))
+    B.append(y)
+
+for i in range(0,5):
+    Result=A[i]+B[i]
+    c.append(Result)
+
+for i in range(0,5):
+    print(c[i]
+
+
+
+
+
+
+          )
---
+++ list.py
@@ -0,0 +1,10 @@
+k=[10,20,30,40,50,60]
+
+for i in range(0,6):
+    k[i]=k[i]+100
+
+for i in range(0,6):
+    print(k[i])
+
+print(type(k))
+print(k)
