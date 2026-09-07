82c43972acf3857c701d649a6a935c50c3482cd6
KOTTEESWARAN2008 <ramusvc957@gmail.com>
2026-09-07 00:45:00 -0700

    Add files via upload

--- Menu programs/Library file.py
+++ Menu programs/Library file.py
@@ -13,14 +13,14 @@ def library_write():
 def library_read():
     F = open("e://BCA1//library.txt", "r")
 
-    print("-" * 60)
-    print("AccNo\tBookName\tBook Author\tNo. of Copies")
-    print("-" * 60)
+    print("-" * 80)
+    print("AccNo\t\tBookName\t\tBook Author\t\tNo. of Copies")
+    print("-" * 80)
 
     for dec in F:
         st = dec.strip().split(",")
 
-        print(f"{st[0]}\t{st[1]}\t\t{st[2]}\t\t{st[3]}")
+        print(f"{st[0]:<10} {st[1]:<25} {st[2]:<25} {st[3]:<20}")
 
     print("-" * 60)
 
